48d42515f7c53027fa1bf17ddb0ab88e745c0953
Adrian Rangel <adrian.rangel@gmail.com>
2016-02-18 14:15:39 -0600

    root is new article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'articles#new'
   resources :articles
   resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
