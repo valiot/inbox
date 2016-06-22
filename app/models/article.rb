dd51ce57772b2e5518eaccca88480cd8f9eec4e4
Balbina <balbinasantana@gmail.com>
2016-06-22 14:01:12 -0500

    Resize with mini_magick and crop images with CSS

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,19 +13,11 @@ class Article < ActiveRecord::Base
 
   private
 
-  def parse_link
-    page = MetaInspector.new(link)
-    self.description = page.description
-    self.title = page.title
-    self.length = page.body_length
-    self.image = upload_to_s3(page.images.best)
-  end
+  def crop_image_and_upload_to_s3(url)
+    image = MiniMagick::Image.open(url)
+    image.resize "360x240"
+    image
 
-  def set_issue
-    self.issue = Issue.current_issue
-  end
-
-  def upload_to_s3(url)
     uri = URI.parse(url)
     filename, extension = uri.path.split('/')[-1].split('.')[-2..-1]
     service = S3::Service.new(access_key_id: ENV['AWS_KEY'],
@@ -33,9 +25,22 @@ class Article < ActiveRecord::Base
 
     bucket = service.buckets.find(ENV['S3_BUCKET'])
     new_object = bucket.objects.build("#{filename}-#{SecureRandom.hex}.#{extension}")
-    new_object.content = open(url)
+    new_object.content = open(image.path)
 
     return new_object.url if new_object.save
     false
   end
+
+  def parse_link
+    page = MetaInspector.new(link)
+    self.description = page.description
+    self.title = page.title
+    self.length = page.body_length
+    self.image = crop_image_and_upload_to_s3(page.images.best)
+  end
+
+  def set_issue
+    self.issue = Issue.current_issue
+  end
+
 end
